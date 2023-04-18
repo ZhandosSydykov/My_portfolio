d74595aa4e1221d7ec6b97b1f95a3bd798c93e76
obito20022004 <obitorobertson@gmail.com>
2023-04-18 23:51:43 +0600

    portfolio

--- index.js
+++ index.js
@@ -36,3 +36,55 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+
+
+// Получаем элементы формы
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const messageInput = document.querySelector('#message');
+
+// Обрабатываем событие отправки формы
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // Отменяем стандартное поведение формы
+
+  // Получаем значения полей ввода
+  const name = nameInput.value.trim();
+  const email = emailInput.value.trim();
+  const message = messageInput.value.trim();
+
+  // Формируем текст сообщения для отправки в Телеграм
+  const CHAT_ID = "-1001930370704";
+  const BOT_TOKEN = "5670394037:AAHDZUPMr-KSqkxL-H4sNUuvV8K_71Z1mvo";
+  const text = `Новое сообщение с сайта:\nИмя: ${name}\nEmail: ${email}\nСообщение: ${message}`;
+
+  // Отправляем сообщение в Телеграм через API бота
+  const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+  const data = {
+    chat_id: CHAT_ID,
+    text: text,
+  };
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log('Сообщение отправлено в Телеграм!');
+      } else {
+        console.error('Ошибка отправки сообщения в Телеграм:', response.status);
+      }
+    })
+    .catch((error) => {
+      console.error('Ошибка отправки сообщения в Телеграм:', error);
+    });
+
+  // Очищаем поля формы
+  nameInput.value = '';
+  emailInput.value = '';
+  messageInput.value = '';
+});
+
